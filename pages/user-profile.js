7d48ee3d92e72dfb9d789a5cdbe93468aaceb4b8
sheng yeong <chia.shengyeong@gmail.com>
2022-08-25 14:48:52 +0800

    getServerSideProps. on demand server side renderring, ideally for privacy, like only owner can see their user profile

---
+++ pages/user-profile.js
@@ -0,0 +1,18 @@
+// this cannot use static renderring because you only want the specific user to be able to access. 
+// hence this needs to be renderred on demand, when the right user is requesting it
+
+function UserProfilePage(props) {
+  return (
+    <h1>{props.username}</h1>
+  )
+}
+
+export default UserProfilePage
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      username: "Max"
+    }
+  }
+}
