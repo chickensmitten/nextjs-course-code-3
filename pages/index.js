e723ebbf5118860c035a5b6bc4fc881bbdb5869e
sheng yeong <chia.shengyeong@gmail.com>
2022-08-25 11:38:39 +0800

    configuration options for getStaticProps. notFound, redirect

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,18 @@ export async function getStaticProps() {
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data"
+      }
+    }
+  }
+
+  if (data.products.length === 0) {
+    return { notFound:true };
+  }
+
   return {
     props: {
       products: data.products
